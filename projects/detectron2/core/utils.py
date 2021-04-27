f003016cabe8d49447c56fbefe988d37b17c25d7
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-04-27 13:28:05 -0400

    Adding changes to test

--- projects/detectron2/core/utils.py
+++ projects/detectron2/core/utils.py
@@ -1,11 +1,17 @@
 # --------------------------------------------------------------------------
 # Utilities directory for slump detection models generation.
 # --------------------------------------------------------------------------
-import os
-from tqdm import tqdm
-import random            # for random integers
-import numpy as np       # for arrays modifications
-import imageio
+import os                  # for os utilities
+import sys                 # for os utilities
+from tqdm import tqdm      # for progress bar
+import datetime            # for dates manipulation
+import glob                # for local files manipulation
+import json                # for json handling
+import random              # for random integers
+import numpy as np         # for arrays modifications
+import imageio             # for managing images
+import pycococreatortools  # for coco dataset resources
+from PIL import Image      # for managing images
 
 __author__ = "Jordan A Caraballo-Vega, Science Data Processing Branch"
 __email__ = "jordan.a.caraballo-vega@nasa.gov"
@@ -67,3 +73,108 @@ def gen_data_png(fimg, img, label, cfg, set='train'):
         # save png images
         imageio.imwrite(f'{save_dir}/{fimg}_img_{i+1}.png', tile_img)
         imageio.imwrite(f'{save_dir}/{fimg}_lbl_{i+1}.png', tile_lab)
+
+
+def gen_coco_dataset(
+        cfg, set='TRAIN', img_reg='*_img_*.png', label_reg='*_lbl_*.png'
+     ):
+
+    data_dir = cfg.DATASET.OUTPUT_DIRECTORY  # root directory
+    input_dir = os.path.join(data_dir, set)  # directory where images reside
+    jsonOut = f'{data_dir}/{cfg.DATASET.DESCRIPTION}_{set}.json'  # output file
+    
+    #tmp_dir = os.path.join(
+    #    os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tmp'))
+    #)
+
+    print(cfg.DATASET.COCO_METADATA.INFO, type(cfg.DATASET.COCO_METADATA.INFO))
+    """
+    if not os.path.isfile(jsonOut):
+
+        # src: https://patrickwasp.com/create-your-own-coco-style-dataset/
+        INFO = {
+            "description": "Slump Detection in World View VHR Imagery",
+            "url": "https://www.nccs.nasa.gov",
+            "version": "0.1.0",
+            "year": "2021",
+            "contributor": "Jordan A. Caraballo-Vega",
+            "date_created": datetime.datetime.utcnow().isoformat(' ')
+        }
+
+        LICENSES = [
+            {
+                "id": 1,
+                "name": "World View Maxar Agreement",
+                "url": "https://www.nccs.nasa.gov"
+            }
+        ]
+
+        CATEGORIES = [
+            {
+                "id": 1,
+                "name": "Slump",
+                "supercategory": "slump"
+            }
+        ]
+
+        classId = 1  # only have slumps for now
+        isCrowd = False
+        categoryInfo = {
+            'id': classId,
+            'is_crowd': isCrowd,
+        }
+
+        train_names = sorted(glob.glob(f'{input_dir}/{img_reg}'))
+        mask_names = sorted(glob.glob(f'{input_dir}/{label_reg}'))
+        print(f"Number of train and mask images: {len(train_names)}")
+
+        images = []
+        annotations = []
+
+        pastId = 0
+
+        # go through each image
+        for curImgName, curMaskName in zip(train_names, mask_names):
+
+            curImgFile = curImgName
+            curMaskFile = curMaskName
+            print(curImgFile, curMaskFile)
+
+            # taking care of the images
+            curImg = Image.open(curImgFile)
+            curImgId = pastId + 1  # make sure it's properly unique
+            pastId = curImgId
+            curImgInfo = pycococreatortools.create_image_info(
+                curImgId, os.path.basename(curImgFile), curImg.size
+            )
+            images.append(curImgInfo)
+
+            # taking care of the annotations
+            curAnnotationId = str(curImgId)
+            binaryMask = np.asarray(
+                Image.open(curMaskFile).convert('1')
+            ).astype(np.uint8)
+
+            annotationInfo = pycococreatortools.create_annotation_info(
+                curAnnotationId, curImgId, categoryInfo, binaryMask,
+                curImg.size, tolerance=2
+            )
+
+            if annotationInfo is not None:
+                annotations.append(annotationInfo)
+            else:
+                print('\t\tNo annotationInfo found for: ' + curMaskName)
+
+        cocoInfo = {
+            "info": INFO,
+            "licenses": LICENSES,
+            "categories": CATEGORIES,
+            "images": images,
+            "annotations": annotations,
+        }
+
+        with open(jsonOut, 'w') as f:
+            f.write(json.dumps(cocoInfo))
+    else:
+        sys.exit(f'{jsonOut} already exists. Please remove it.')
+    """
