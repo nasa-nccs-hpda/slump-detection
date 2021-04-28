b1bd688bcd1d29b8ed7c208fe80bd60e0ad6b46a
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-04-28 00:29:46 -0400

    Slurm changes

--- projects/detectron2/gen_dataset.sh
+++ projects/detectron2/gen_dataset.sh
@@ -5,4 +5,4 @@
 #SBATCH -e slurm-%j.err
 
 # Input: container location given as a parameter on the command line
-singularity exec -B $NOBACKUP:$NOBACKUP $1 python gen_dataset.py -c config/slump_mask_rcnn_R_50_FPN_3x.yaml
+singularity exec -B $NOBACKUP:$NOBACKUP $1 python $NOBACKUP/slump-detection/projects/detectron2/gen_dataset.py -c config/slump_mask_rcnn_R_50_FPN_3x.yaml
