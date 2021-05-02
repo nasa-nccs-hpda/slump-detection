0e9ec980b2c3948e2f4f92701a62d11b656d9aeb
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-05-02 09:44:24 -0400

    Batch prediciton setup

--- projects/detectron2/core/utils.py
+++ projects/detectron2/core/utils.py
@@ -209,9 +209,10 @@ def predict_windowing(x, model, config):
         n_channels, extended_height, extended_width)#, device=torch.device('cpu')
     #)
 
-    """
+
     # fill extended image with mirrors:
     ext_x[:, :img_height, :img_width] = x
+    """
     for i in range(img_height, extended_height):
         ext_x[:, i, :] = ext_x[:, 2 * img_height - i - 1, :]
     for j in range(img_width, extended_width):
