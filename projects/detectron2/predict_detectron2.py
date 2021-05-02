fa73623306ebc349496ed2571a5fcae7ddc3e6ff
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-05-02 19:41:12 -0400

    Batch prediciton setup

--- projects/detectron2/predict_detectron2.py
+++ projects/detectron2/predict_detectron2.py
@@ -154,7 +154,7 @@ def run(cfg):
             # ME QUEDE AQUI
             prediction = predict_batch(x_data=x_data, model=model, config=cfg)
             # print("Prediction shape", prediction.shape, prediction.min(), prediction.max())
-            prediction[prediction > 8] = 1
+            prediction[prediction > 15] = 1
             prediction[prediction > 1] = 0
             prediction = prediction.astype(np.int8)  # type to int16
 
