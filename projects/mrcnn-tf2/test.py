c0c239a40b7805a5c5841002f9301f2d71a629cb
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-09-02 19:56:05 -0400

    Adding missing evaluate

--- projects/mrcnn-tf2/test.py
+++ projects/mrcnn-tf2/test.py
@@ -38,3 +38,32 @@ LOGS_DIRECTORY = os.path.join(ROOT_DIR, "logs")
 print(f"ROOT_DIR: {ROOT_DIR}")
 print(f"PRETRAINED_MODEL_PATH: {PRETRAINED_MODEL_PATH}")
 print(f"LOGS_DIRECTORY: {LOGS_DIRECTORY}")
+
+
+class MappingChallengeConfig(Config):
+    """Configuration for training on data in MS COCO format.
+    Derives from the base Config class and overrides values specific
+    to the COCO dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "crowdai-mapping-challenge"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 5
+
+    # Uncomment to train on 8 GPUs (default is 1)
+    GPU_COUNT = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # 1 Backgroun + 1 Building
+
+    STEPS_PER_EPOCH=1000
+    VALIDATION_STEPS=50
+
+
+    IMAGE_MAX_DIM=320
+    IMAGE_MIN_DIM=320
+
+config = MappingChallengeConfig()
+config.display()
